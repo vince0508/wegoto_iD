688fae0a65e626636d35d1223088df93a8aa9e1f
Mesnier Vincent <vincent_mesnier@yahoo.fr>
2016-06-24 16:02:52 +0200

    test

--- dist/iD.js
+++ dist/iD.js
@@ -35422,10 +35422,10 @@ iD.ui.EntityEditor = function(context) {
         $enter.append('div')
             .attr('class', 'inspector-border inspector-preset');
 
-//SUP TAG
-         /* $enter.append('div')
+
+          $enter.append('div')
               .attr('class', 'inspector-border raw-tag-editor inspector-inner');
-  */
+  
 
         $enter.append('div')
             .attr('class', 'inspector-border raw-member-editor inspector-inner');
